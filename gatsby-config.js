a24012afd5ccbd95e6ebd513b284dd4166157d30
Hoonjae Lee <hoonjae@artlab.ai>
2020-07-19 23:05:54 +0900

    fix:remove draft plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,7 +39,6 @@ module.exports = {
           `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
-          `gatsby-plugin-draft`,
           `gatsby-plugin-sitemap`,
         ],
       },
